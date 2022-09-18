a31e3369c7ed697a1e8095c3147a1d1e5bd4c739
Ej <elnesjanisidoro416@gmail.com>
2022-09-18 13:08:58 +0800

    adding new project - QR code generator

---
+++ index.js
@@ -0,0 +1,47 @@
+const container = document.querySelector('.container');
+const generateBtn = document.querySelector('.form-btn');
+const qrInput = document.querySelector('#qrInput');
+const qrImg = document.getElementById('qr-img');
+const download = document.getElementById('download-btn');
+
+let toDownloadSrc = '';
+generateBtn.addEventListener('click', (e) =>{
+    e.preventDefault();
+
+    let qrValue = qrInput.value;
+    if(!qrValue) return;
+    generateBtn.innerText = 'Generating your QR Code...';
+    generateBtn.style.backgroundColor = 'rgba(67, 212, 38, 0.722)';
+    let src =  `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue}`;
+    qrImg.src = src;
+    toDownloadSrc = src;
+    qrImg.addEventListener('load', () => {
+        container.classList.add('active');
+        generateBtn.innerText = 'Generate QR Code';
+        generateBtn.style.backgroundColor = 'rgba(98, 181, 249, 0.722)';
+        qrInput.value = '';
+    });
+
+});
+
+download.addEventListener('click', (e) => {
+    e.preventDefault();
+    download.innerText = 'Downloading your file...';
+    download.style.backgroundColor = 'rgba(67, 212, 38, 0.722)';
+    fetchFile(toDownloadSrc);
+});
+
+function fetchFile(url){
+    fetch(url).then(res => res.blob()).then(file => {
+        let tempUrl = URL.createObjectURL(file);
+        let aTag = document.createElement('a');
+        aTag.href = tempUrl;
+        aTag.download = url.replace(/^.*[\\\/]/,'');
+        document.body.appendChild(aTag);
+        aTag.click();
+        aTag.remove();
+        URL.revokeObjectURL(tempUrl);
+        download.innerText = 'Download QR Code';
+        download.style.backgroundColor = 'rgb(249, 221, 98)';
+    });
+}
